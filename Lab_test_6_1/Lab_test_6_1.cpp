e04ad32f21b0c12adf20986635bf98d4a08d07e5
Maksym Danyliuk <maksym.danyliuk.itis.2020@lpnu.ua>
2021-05-21 12:49:23 +0300

    Lab_6_1 first commit

---
+++ Lab_test_6_1/Lab_test_6_1.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_6_1/Array.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Labtest61
+{
+	TEST_CLASS(Labtest61)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int n = 4;
+			Array c = Array(n);
+			Array::value_type* a = new Array::value_type[n];
+			for (int i = 0; i < n; i++)
+				a[i] = 3;
+			Array::iterator l = const_cast<Array::iterator>(c.begin());
+			for (int j = 0; j < n; j++, l++)
+				*l = a[j];
+
+			int sum = c.Sum();
+			Assert::AreEqual(sum, 12);
+		}
+	};
+}
